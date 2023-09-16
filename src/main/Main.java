2e8b9e8b26611ad07ca7c5039eb72cf9504f3bc1
Wolf <caffeinate@msn.com>
2023-09-16 17:06:04 -0400

    Add console based interface
    Utilize reflection to capture available methods

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,79 @@
 package main;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        boolean test = StringTools.palindromeIterative("racecar");
-        System.out.println(test);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Wolf's string utility program!");
+        System.out.println("To begin, select a mode from the following options:");
+
+        Class<StringTools> stringToolsClass = StringTools.class;
+        Method[] stringToolsMethods = stringToolsClass.getDeclaredMethods();
+
+        // Print out all the available methods for the user to choose from using reflection.
+        for (int i = 0; i < stringToolsMethods.length; i++)
+            System.out.println("[" + (i + 1) + "]" + " - " + stringToolsMethods[i].getName());
+
+        // Get the user's choice of method to use. User was presented options in index-1, so we must shift down the
+        // index by 1 to return to index-0.
+        int userInput = input.nextInt() - 1;
+        try {
+            Method chosenMethod = stringToolsMethods[userInput];
+            Parameter[] chosenMethodParams = chosenMethod.getParameters();
+            String[] chosenMethodParamStrings = new String[chosenMethodParams.length];
+            for (int i = 0; i < chosenMethodParams.length; i++)
+                chosenMethodParamStrings[i] = (
+                        chosenMethodParams[i].getName() + " - " + chosenMethodParams[i].getType().getName()
+                );
+
+            System.out.println();
+            System.out.println(
+                    "Great choice! This method requires " +
+                    chosenMethodParams.length +
+                    " param" + (chosenMethodParams.length > 0 ? "" : "s") + ":"
+            );
+            for (String chosenMethodNameString : chosenMethodParamStrings)
+                System.out.println(chosenMethodNameString);
+
+            System.out.println();
+            System.out.println("Please enter them one at a time as prompted.");
+
+            // Consume the lingering \n that .nextInt() did not consume.
+            input.nextLine();
+
+            String[] arguments = new String[chosenMethodParams.length];
+            for (int i = 0; i < chosenMethodParams.length; i++) {
+                System.out.println("Enter parameter #" + (i + 1) + " and then press enter.");
+                arguments[i] = input.nextLine();
+            }
+
+            System.out.println("Alright, executing method with provided params...");
+            try {
+                System.out.println(Arrays.toString(arguments));
+                System.out.println();
+
+                String output = chosenMethod.invoke((new StringTools()), arguments).toString();
+
+                System.out.println("Success! The output is \"" + output + "\"");
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println("Arguments passed were somehow wrong.");
+            }
+            catch (IllegalAccessException e) {
+                System.out.println("Unable to access method. Weird.");
+            }
+            catch (InvocationTargetException e) {
+                System.out.println("An unknown error occurred.");
+            }
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("Whoops, you've chosen a method that doesn't exist. Please try again.");
+        }
     }
 }
